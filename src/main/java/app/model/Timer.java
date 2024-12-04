ff33f77efeb10825c1d5c968fb02f860ae468fd7
s1thedj <shuff1ee.beatz@gmail.com>
2024-12-04 18:17:29 +0300

    task3

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,6 +1,16 @@
 package app.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Timer {
+
+    public Timer getTimer() {
+        return new Timer();
+    }
 
     private Long nanoTime = System.nanoTime();
 
